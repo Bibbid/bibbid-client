2caaa7b71e449bbb55fa07a8a7da94fbf6e30a24
SWARVY <swarvy0826@naver.com>
2025-08-21 00:26:46 +0900

    버튼 컴포넌트 경로 수정 및 Foundation 페이지를 TestPage로 변경, 테스트 페이지 구성 요소 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
 import { router, Stack } from 'expo-router';
-
 import { View } from 'react-native';
-import { Button, ButtonText } from '~/shared/ui/button';
+import { Button, ButtonText } from '~/shared/ui/button/button';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Home' }} />
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Button variant="solid-gray" size="md" onPress={() => router.push('/foundation')}>
+        <Button
+          variant="solid-gray"
+          size="md"
+          onPress={() => router.push('/foundation')}>
           <ButtonText variant="solid-gray" size="md">
             Go to Foundation
           </ButtonText>
