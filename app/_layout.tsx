94acc36184311fc9db2ffab49174cc210588ce9d
SWARVY <swarvy0826@naver.com>
2025-09-14 09:58:24 +0900

    레이아웃에 SafeAreaView 추가 및 바텀 내비게이션과 탑 내비게이션 통합

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Stack } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { DevToolsBubble } from 'react-native-react-query-devtools';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { StyleSheet } from 'react-native-unistyles';
 import { BottomSheetOverlayProvider } from '~/shared/ui/bottom-sheet';
 import { Toast } from '~/shared/ui/toast';
@@ -14,15 +15,17 @@ export default function Layout() {
     <GestureHandlerRootView style={styles.container}>
       <QueryClientProvider client={queryClient}>
         <BottomSheetOverlayProvider>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              contentStyle: styles.container,
-            }}>
-            <Stack.Screen name="index" options={{ title: 'Home' }} />
-          </Stack>
-          <Toast />
-          {__DEV__ && <DevToolsBubble queryClient={queryClient} />}
+          <SafeAreaView edges={['top', 'left', 'right']} style={{ flex: 1 }}>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+                contentStyle: styles.container,
+              }}>
+              <Stack.Screen name="index" options={{ title: 'Home' }} />
+            </Stack>
+            <Toast />
+            {__DEV__ && <DevToolsBubble queryClient={queryClient} />}
+          </SafeAreaView>
         </BottomSheetOverlayProvider>
       </QueryClientProvider>
     </GestureHandlerRootView>
--- app/foundation/index.tsx
+++ app/foundation/index.tsx
@@ -1,5 +1,21 @@
+import { Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { TestPage } from '~/pages/test';
+import { TopNavigation } from '~/shared/ui/navigation';
 
 export default function Foundation() {
-  return <TestPage />;
+  const { bottom } = useSafeAreaInsets();
+
+  return (
+    <View style={{ flex: 1, paddingBottom: bottom }}>
+      <TopNavigation
+        left={<Text style={{ color: 'white', fontSize: 16 }}>Left</Text>}
+        center={
+          <Text style={{ color: 'white', fontSize: 16 }}>Foundation</Text>
+        }
+        right={<Text style={{ color: 'white', fontSize: 16 }}>Right</Text>}
+      />
+      <TestPage />
+    </View>
+  );
 }
