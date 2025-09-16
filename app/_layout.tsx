943113bfc72b1724590f992e7dd801b52f88d3c0
SWARVY <swarvy0826@naver.com>
2025-09-16 22:50:46 +0900

    레이아웃에 AuthLoaded 컴포넌트 추가 및 Foundation 컴포넌트에서 로그아웃 버튼 구현

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,6 +5,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { DevToolsBubble } from 'react-native-react-query-devtools';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StyleSheet } from 'react-native-unistyles';
+import { AuthLoaded } from '~/shared/auth';
 import { BottomSheetOverlayProvider } from '~/shared/ui/bottom-sheet';
 import { Toast } from '~/shared/ui/toast';
 
@@ -15,25 +16,26 @@ const queryClient = new QueryClient();
  * - https://github.com/expo/expo/issues/33108
  * - https://github.com/expo/expo/issues/33673
  */
-
 export default function Layout() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <QueryClientProvider client={queryClient}>
         <BottomSheetOverlayProvider>
-          <SafeAreaView
-            edges={['top', 'left', 'right']}
-            style={styles.container}>
-            <Stack
-              screenOptions={{
-                headerShown: false,
-                contentStyle: styles.container,
-              }}>
-              <Stack.Screen name="index" options={{ title: 'Home' }} />
-            </Stack>
-            <Toast />
-            {__DEV__ && <DevToolsBubble queryClient={queryClient} />}
-          </SafeAreaView>
+          <AuthLoaded>
+            <SafeAreaView
+              edges={['top', 'left', 'right']}
+              style={styles.container}>
+              <Stack
+                screenOptions={{
+                  headerShown: false,
+                  contentStyle: styles.container,
+                }}>
+                <Stack.Screen name="index" options={{ title: 'Home' }} />
+              </Stack>
+              <Toast />
+              {__DEV__ && <DevToolsBubble queryClient={queryClient} />}
+            </SafeAreaView>
+          </AuthLoaded>
         </BottomSheetOverlayProvider>
       </QueryClientProvider>
     </GestureHandlerRootView>
