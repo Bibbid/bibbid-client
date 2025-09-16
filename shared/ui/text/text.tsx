cc6cdb7c166024d6f147a38b9784b9b94b5fced3
SWARVY <swarvy0826@naver.com>
2025-09-16 22:44:52 +0900

    CustomText 컴포넌트 수정: 폰트 가중치 제거 및 기본 폰트 'Montserrat'로 설정

--- shared/ui/text/text.tsx
+++ shared/ui/text/text.tsx
@@ -1,35 +1,5 @@
 import { Text as RNText, TextProps as RNTextProps } from 'react-native';
 
-export interface CustomTextProps extends RNTextProps {
-  weight?:
-    | '100'
-    | '200'
-    | '300'
-    | '400'
-    | '500'
-    | '600'
-    | '700'
-    | '800'
-    | '900';
-}
-
-const fontWeights = {
-  '100': 'Montserrat-Thin',
-  '200': 'Montserrat-ExtraLight',
-  '300': 'Montserrat-Light',
-  '400': 'Montserrat-Regular',
-  '500': 'Montserrat-Medium',
-  '600': 'Montserrat-SemiBold',
-  '700': 'Montserrat-Bold',
-  '800': 'Montserrat-ExtraBold',
-  '900': 'Montserrat-Black',
-};
-
-export function CustomText({ style, weight, ...props }: CustomTextProps) {
-  return (
-    <RNText
-      style={[style, { fontFamily: fontWeights[weight ?? '400'] }]}
-      {...props}
-    />
-  );
+export function CustomText({ style, ...props }: RNTextProps) {
+  return <RNText style={[style, { fontFamily: 'Montserrat' }]} {...props} />;
 }
