c7387a0d51e80113ebeabdf74260eda187cbfd5c
SWARVY <swarvy0826@naver.com>
2025-09-17 22:31:26 +0900

    [피드] 스크롤 뷰 추가 및 MasonryGrid 컴포넌트로 레이아웃 개선 / 상태 표시줄 스타일 추가

--- app/(authorized)/feed/index.tsx
+++ app/(authorized)/feed/index.tsx
@@ -1,16 +1,74 @@
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
 import { ColorPalette } from '~/pages/feed';
+import { MasonryGrid, MasonryGridItem } from '~/shared/ui/masonry-grid';
 import { TopNavigation } from '~/shared/ui/navigation';
 import { CustomText } from '~/shared/ui/text';
+
+const TEST_DATA = [
+  {
+    id: 1,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 2,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 3,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 4,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 5,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 6,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 7,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 8,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 9,
+    src: 'https://picsum.photos/200/300',
+  },
+  {
+    id: 10,
+    src: 'https://picsum.photos/200/300',
+  },
+];
 
 export default function Feed() {
   return (
     <View style={styles.container}>
-      <TopNavigation
-        left={<CustomText style={styles.title}>Be Inspired</CustomText>}
-      />
-      <ColorPalette />
+      <View>
+        <TopNavigation
+          left={<CustomText style={styles.title}>Be Inspired</CustomText>}
+        />
+        <ColorPalette />
+      </View>
+      <ScrollView style={{ flex: 1 }}>
+        <MasonryGrid
+          data={TEST_DATA}
+          renderItem={({ item, index }) => (
+            <MasonryGridItem
+              item={item}
+              index={index}
+              total={TEST_DATA.length}
+            />
+          )}
+        />
+      </ScrollView>
     </View>
   );
 }
@@ -18,6 +76,9 @@ export default function Feed() {
 const styles = StyleSheet.create((theme) => ({
   container: {
     flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    rowGap: 16,
   },
   title: {
     color: 'white',
