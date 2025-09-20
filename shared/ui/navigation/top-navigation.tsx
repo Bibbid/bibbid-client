dfd2dc0805851a1bfd6034880c7cd5fbc2d92b7b
SWARVY <swarvy0826@naver.com>
2025-09-20 17:00:30 +0900

    TopNavigation 컴포넌트에 스타일 속성 추가 및 View 스타일 통합

--- shared/ui/navigation/top-navigation.tsx
+++ shared/ui/navigation/top-navigation.tsx
@@ -2,18 +2,24 @@ import { CustomText } from '../text';
 import { topNavigationStyles } from './top-navigation.styles';
 import { useRouter } from 'expo-router';
 import { ChevronLeft } from 'lucide-react-native';
-import { Pressable, View } from 'react-native';
+import { Pressable, type StyleProp, View, type ViewStyle } from 'react-native';
 import { useAnimatedTheme } from 'react-native-unistyles/reanimated';
 
 interface TopNavigationProps {
+  style?: StyleProp<ViewStyle>;
   left?: React.ReactNode;
   center?: React.ReactNode;
   right?: React.ReactNode;
 }
 
-export function TopNavigation({ left, center, right }: TopNavigationProps) {
+export function TopNavigation({
+  left,
+  center,
+  right,
+  style,
+}: TopNavigationProps) {
   return (
-    <View style={topNavigationStyles.container}>
+    <View style={[topNavigationStyles.container, style]}>
       {left ? left : <BlankNavItem />}
       {center ? center : <BlankNavItem />}
       {right ? right : <BlankNavItem />}
