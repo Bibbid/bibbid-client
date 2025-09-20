c1255678c00f71f5177efc3ada36db36661792c5
SWARVY <swarvy0826@naver.com>
2025-09-20 16:01:48 +0900

    쿼리 키를 정의하는 새로운 파일 추가: 토큰, 색상, 프로필 관련 API 쿼리 키 포함

---
+++ shared/api/query-keys.ts
@@ -0,0 +1,11 @@
+export const queryKeys = {
+  token: {
+    'get-my-tokens': ['token', 'get-my-tokens'],
+  },
+  color: {
+    'get-colors': ['color', 'get-colors'],
+  },
+  profile: {
+    'get-my-profile': ['profile', 'get-my-profile'],
+  },
+};
