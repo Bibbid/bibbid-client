6035a099ee8961442581350aa41619b7274f0974
SWARVY <swarvy0826@naver.com>
2025-09-20 16:58:43 +0900

    SVG 파일을 React 컴포넌트로 처리하기 위한 타입 선언 추가 및 Metro 구성 파일 수정, react-native-svg-transformer 패키지 추가

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,25 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const {
+  wrapWithReanimatedMetroConfig,
+} = require('react-native-reanimated/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
+const config = () => {
+  const config = getDefaultConfig(__dirname);
 
-const config = getDefaultConfig(__dirname);
+  const { transformer, resolver } = wrapWithReanimatedMetroConfig(config);
 
+  config.transformer = {
+    ...transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+  };
+  config.resolver = {
+    ...resolver,
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
+  };
+
+  return config;
+};
 module.exports = config;
