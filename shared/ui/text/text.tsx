843a130575b7459ae0c500fc2b7df55308ad4534
SWARVY <swarvy0826@naver.com>
2025-09-20 13:25:53 +0900

    CustomText 컴포넌트의 스타일 순서를 변경하여 Montserrat 폰트가 우선 적용되도록 수정

--- shared/ui/text/text.tsx
+++ shared/ui/text/text.tsx
@@ -1,5 +1,5 @@
 import { Text as RNText, TextProps as RNTextProps } from 'react-native';
 
 export function CustomText({ style, ...props }: RNTextProps) {
-  return <RNText style={[style, { fontFamily: 'Montserrat' }]} {...props} />;
+  return <RNText style={[{ fontFamily: 'Montserrat' }, style]} {...props} />;
 }
