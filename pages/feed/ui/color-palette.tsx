bc2c9a1a9646a4e3ec739039a3be011daa138c87
SWARVY <swarvy0826@naver.com>
2025-09-28 01:14:40 +0900

    색상 팔레트 파일에서 FlashList를 '@shopify/flash-list'에서 가져오도록 수정

--- pages/feed/ui/color-palette.tsx
+++ pages/feed/ui/color-palette.tsx
@@ -1,3 +1,4 @@
+import { FlashList } from '@shopify/flash-list';
 import { SuspenseQuery } from '@suspensive/react-query';
 import { Pressable, View } from 'react-native';
 import { StyleSheet } from 'react-native-unistyles';
@@ -5,7 +6,6 @@ import { create } from 'zustand';
 import Dot from '~/assets/icons/dot-solid.svg';
 import { getColorsOptions } from '~/entities/color';
 import { Chip } from '~/shared/ui/chip';
-import { FlashList } from '~/shared/ui/flash-list';
 
 const ALL_COLOR = {
   displayName: 'All',
