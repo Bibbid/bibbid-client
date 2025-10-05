7abc62e0b26065e2e6c08fc35785e3396b50416b
SWARVY <swarvy0826@naver.com>
2025-10-06 02:12:49 +0900

    StatusBar를 expo-status-bar로 변경하고 레이아웃 개선

--- app/(unauthorized)/_layout.tsx
+++ app/(unauthorized)/_layout.tsx
@@ -1,11 +1,13 @@
 import ErrorScreen from '../error';
 import { ErrorBoundary } from '@suspensive/react';
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 import { Unauthorized } from '~/shared/auth';
 
 export default function UnauthorizedLayout() {
   return (
     <ErrorBoundary fallback={<ErrorScreen />}>
+      <StatusBar style="light" />
       <Unauthorized>
         <Stack screenOptions={{ headerShown: false }} />
       </Unauthorized>
